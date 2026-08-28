65e60aea66eb93582a451691dae116bb24e6eae3
Alex Jamshidi <alexjamshidi@gmail.com>
2026-08-28 19:09:00 +0100

    refactored cat

--- implement-shell-tools/cat/cat.js
+++ implement-shell-tools/cat/cat.js
@@ -1,83 +1,98 @@
 const fs = require("fs");
 
-const arguments = process.argv;
-const userArguments = arguments.slice(2);
+//
+// ===== cat Procedure =====
+function cat(args) {
+  const userArgs = args.slice(2);
+  const currentWorkingDirectory = getCurrentWorkingDirectory(args);
+  const fileNames = getFileNames(userArgs, currentWorkingDirectory);
 
-const index = arguments[1].lastIndexOf("/");
-const currentWorkingDirectory = arguments[1].slice(0, index + 1);
+  const allFilesContents = readFiles(fileNames, currentWorkingDirectory);
+  executeFlags(getFlags(userArgs), allFilesContents);
+  printLines(allFilesContents);
+}
 
-const flags = userArguments
-  .filter((argument) => argument.startsWith("-"))
-  .map((argument) => argument.slice(1))
-  .join("");
+//
+// ===== Extracting Data From Arguments =====
+function getCurrentWorkingDirectory(args) {
+  const index = args[1].lastIndexOf("/");
+  return args[1].slice(0, index + 1);
+}
 
+function getFileNames(userArgs) {
+  return userArgs.filter((argument) => !argument.startsWith("-"));
+}
+
+function readFiles(fileNames, currentWorkingDirectory) {
+  const allFilesContents = [];
+  fileNames.forEach((fileName) => {
+    fileContent = readFile(fileName, currentWorkingDirectory);
+    allFilesContents.push(fileContent.split("\n"));
+  });
+  return allFilesContents;
+}
+
+function readFile(fileName, currentWorkingDirectory) {
+  const filePath = currentWorkingDirectory + fileName;
+  return fs.readFileSync(filePath, "utf8").trimEnd();
+}
+
+//
+// ===== Flag Handling =====
 const flagHandlers = {
   b: bFlag,
   n: nFlag,
 };
 
-const fileNames = userArguments.filter((argument) => !argument.startsWith("-"));
-let allFilesContents = [];
-
-readFiles();
-executeFlags();
-printLines();
-
-function readFiles() {
-  fileNames.forEach((fileName) => {
-    fileContent = readFile(fileName);
-    allFilesContents.push(fileContent.split("\n"));
-  });
+function getFlags(userArgs) {
+  return userArgs
+    .filter((arg) => arg.startsWith("-"))
+    .map((arg) => arg.slice(1))
+    .join("");
 }
 
-function readFile(fileName) {
-  const filePath = currentWorkingDirectory + fileName;
-  return fs.readFileSync(filePath, "utf8").trimEnd();
-}
-
-function executeFlags() {
+function executeFlags(flags, allFilesContents) {
   for (const flag of flags) {
-    if (flagHandlers[flag]) {
-      allFilesContents = flagHandlers[flag]();
-    } else {
-      console.error(`cat: illegal option -- ${flag}\nusage: cat [-belnstuv] [file ...]`);
-      process.exit(1);
-    }
+    if (flagHandlers[flag]) flagHandlers[flag](allFilesContents, flags);
+    else invalidFlagError();
   }
 }
 
-function bFlag() {
-  const resultFiles = [];
+function invalidFlagError() {
+  console.error(`cat: illegal option -- ${flag}\nusage: cat [-belnstuv] [file ...]`);
+  process.exit(1);
+}
 
-  for (const fileContent of allFilesContents) {
-    const formattedLines = [];
-    let lineNumber = 1;
-
-    for (const line of fileContent) {
-      if (line === "") {
-        formattedLines.push(line);
-      } else {
-        const formattedLine = `${String(lineNumber++).padStart(6, " ")} ${line}`;
-        formattedLines.push(formattedLine);
+function bFlag(allFilesContents) {
+  let linenumber = 1;
+  allFilesContents.forEach((fileContent) => {
+    fileContent.forEach((line, index) => {
+      if (line !== "") {
+        fileContent[index] = `${String(linenumber).padStart(6, " ")} ${line}`;
+        linenumber += 1;
       }
-    }
-
-    resultFiles.push(formattedLines);
-  }
-
-  return resultFiles;
-}
-
-function nFlag() {
-  return allFilesContents.map((fileContent) => {
-    return fileContent.map((line, index) => `${String(index + 1).padStart(6, " ")} ${line}`);
+    });
   });
 }
 
-function printLines() {
+function nFlag(allFilesContents, flags) {
+  if (!flags.includes("b")) {
+    allFilesContents.forEach((fileContent) => {
+      fileContent.forEach((line, index) => (fileContent[index] = `${String(index + 1).padStart(6, " ")} ${line}`));
+    });
+  }
+}
+
+//
+// ==== Print Output =====
+function printLines(allFilesContents) {
   allFilesContents.forEach((fileContent) => {
     fileContent.forEach((line) => {
       console.log(line);
     });
   });
 }
+
+//
+// ===== Run cat =====
+cat(process.argv);
