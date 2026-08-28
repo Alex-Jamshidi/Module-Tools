fce72f0c63b83180ab45dc058314a49e83d63d22
Alex Jamshidi <alexjamshidi@gmail.com>
2026-08-28 06:28:08 +0100

    updated ls so that data doesn't rely on global variables and is passed through functions as arguments - also fixed an if boolean

--- implement-shell-tools/ls/ls.js
+++ implement-shell-tools/ls/ls.js
@@ -1,17 +1,6 @@
 const fs = require("fs");
 const { allowedNodeEnvironmentFlags } = require("process");
 
-const args = process.argv;
-const userArgs = args.slice(2);
-
-const index = args[1].lastIndexOf("/");
-const currentWorkingDirectory = args[1].slice(0, index);
-
-const flags = userArgs
-  .filter((arg) => arg.startsWith("-"))
-  .map((arg) => arg.slice(1))
-  .join("");
-
 const flagHandlers = {
   1: Flag1,
   a: Flaga,
@@ -20,81 +9,106 @@ const flagHandlers = {
 let printInList = false;
 let showAll = false;
 
-const fsItems = userArgs.filter((arg) => !arg.startsWith("-"));
-let dirArgs;
-let fileArgs;
-let outputString = "";
+ls(process.argv);
 
-checkArgsLength();
-executeFlags();
-filterFilesAndDirs();
-populateOutput();
-print();
+// ----------
 
-function executeFlags() {
+function ls(args) {
+  const userArgs = getUserArgs(args);
+  const currentWorkingDirectory = getCurrentWorkingDirectory(args);
+  const fsItems = checkArgsLength(getFsItems(userArgs));
+
+  executeFlags(getFlags(userArgs));
+
+  const dirArgs = getDirArgs(fsItems, currentWorkingDirectory);
+  const fileArgs = getFileArgs(fsItems, currentWorkingDirectory);
+
+  print(populateOutput(fsItems, dirArgs, fileArgs, currentWorkingDirectory));
+}
+
+function getUserArgs(args) {
+  return args.slice(2);
+}
+
+function getFsItems(userArgs) {
+  return userArgs.filter((arg) => !arg.startsWith("-"));
+}
+
+function getCurrentWorkingDirectory(args) {
+  const index = args[1].lastIndexOf("/");
+  return args[1].slice(0, index);
+}
+
+function getFlags(userArgs) {
+  return userArgs
+    .filter((arg) => arg.startsWith("-"))
+    .map((arg) => arg.slice(1))
+    .join("");
+}
+
+function executeFlags(flags) {
   for (const flag of flags) {
-    if (flagHandlers[flag]) {
-      allFilesContents = flagHandlers[flag]();
-    } else {
-      console.error(
-        `ls: invalid option -- ${flag}\nusage: ls [-@ABCFGHILOPRSTUWXabcdefghiklmnopqrstuvwxy1%,] [--color=when] [-D format] [file ...]`,
-      );
-      process.exit(1);
-    }
+    if (flagHandlers[flag]) flagHandlers[flag]();
+    else invalidFlagError();
   }
 }
 
-function Flag1() {
-  if (printInList) {
-    outputString = outputString.replaceAll("\t", "\n");
-    outputString = outputString.replaceAll("\n\n", "\n");
-  } else {
-    printInList = true;
-  }
+function invalidFlagError() {
+  console.error(
+    `ls: invalid option -- ${flag}\nusage: ls [-@ABCFGHILOPRSTUWXabcdefghiklmnopqrstuvwxy1%,] [--color=when] [-D format] [file ...]`,
+  );
+  process.exit(1);
+}
+
+function Flag1(outputString) {
+  if (printInList) return outputString.replaceAll("\t", "\n").replaceAll("\n\n", "\n");
+  else printInList = true;
 }
 
 function Flaga() {
   showAll = true;
 }
 
-function checkArgsLength() {
-  if (fsItems.length == 0) {
-    fsItems.push(".");
-  }
+function checkArgsLength(fsItems) {
+  if (fsItems.length == 0) fsItems.push(".");
+  return fsItems;
 }
 
-function filterFilesAndDirs() {
-  dirArgs = fsItems.filter((p) => fs.statSync(currentWorkingDirectory + "/" + p).isDirectory());
-  fileArgs = fsItems.filter((p) => !fs.statSync(currentWorkingDirectory + "/" + p).isDirectory());
-  if (showAll == false) {
-    removeDotFiles(fileArgs);
-  }
+function getDirArgs(fsItems, currentWorkingDirectory) {
+  return fsItems.filter((p) => fs.statSync(currentWorkingDirectory + "/" + p).isDirectory());
 }
 
-function populateOutput() {
+function getFileArgs(fsItems, currentWorkingDirectory) {
+  const fileArgs = fsItems.filter((p) => !fs.statSync(currentWorkingDirectory + "/" + p).isDirectory());
+  if (showAll == false) removeDotFiles(fileArgs);
+  return fileArgs;
+}
+
+function populateOutput(fsItems, dirArgs, fileArgs, currentWorkingDirectory) {
+  let outputString = "";
   if (fsItems.length == 1) {
     fileArgs.forEach((file) => {
       outputString += file + " ";
     });
     dirArgs.forEach((dir) => {
-      outputString += dirOutput(dir);
+      outputString += dirOutput(dir, currentWorkingDirectory);
     });
   } else {
     fileArgs.forEach((file) => {
       outputString += file + "\t";
     });
     dirArgs.forEach((dir) => {
-      outputString += "\n\n" + dir + ":\n" + dirOutput(dir);
+      outputString += "\n\n" + dir + ":\n" + dirOutput(dir, currentWorkingDirectory);
     });
   }
+  return outputString;
 }
 
-function dirOutput(dir) {
+function dirOutput(dir, currentWorkingDirectory) {
   let contents = fs.readdirSync(currentWorkingDirectory + "/" + dir);
   let outputStr = "";
-  if (showAll) {
-    outputStr += ".\t..\t";
-  } else {
+  if (showAll) outputStr += ".\t..\t";
+  else {
     for (let i = contents.length - 1; i >= 0; i--) {
       contents = removeDotFiles(contents);
     }
@@ -107,16 +121,13 @@ function dirOutput(dir) {
 
 function removeDotFiles(fileList) {
   for (let i = fileList.length - 1; i >= 0; i--) {
-    if (fileList[i][0] == ".") {
-      fileList.splice(i, 1);
-    }
+    if (fileList[i][0] == ".") fileList.splice(i, 1);
   }
   return fileList;
 }
 
-function print() {
-  if (printInList) {
-    Flag1();
-  }
-  console.log(outputString.trimEnd());
+function print(outputString) {
+  let output = outputString;
+  if (printInList) output = Flag1(output);
+  console.log(output.trimEnd());
 }
