d44188138744975ca5d9be50c2cbe1d7b8aa7ac6
Alex Jamshidi <alexjamshidi@gmail.com>
2026-09-01 00:35:34 +0100

    exercise 6 completed

---
+++ Sprint 5 Prep Exercises/exercise_6.py
@@ -0,0 +1,4 @@
+# Think of the advantages of using methods instead of free functions. Write them down in your notebook.
+
+# Better readability of code
+# Encapsulation of that function only to class - cleaner and better security.
