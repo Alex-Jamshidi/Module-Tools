fac816e25654d710be9ba30a1392b1354d633809
Alex Jamshidi <alexjamshidi@gmail.com>
2026-09-01 00:25:01 +0100

    exercise 3 complete

--- Sprint 5 Prep Exercises/exercise_2.py
+++ Sprint 5 Prep Exercises/exercise_2.py
@@ -5,7 +5,6 @@ print(double(10))
 
 # Read the above code and write down what the bug is. How would you fix it?
 
-# Potential bug: code says double and it is multiplying by 3
-# We can't be sure this is a bug without knowing more information about intent of the developer or requirement of the user.
+# Potential bug: Code says double and it is multiplying by 3
 
-# Assuming a bug; it could be fixed by changing the name of the function to triple, or the integer to 2.
+# It could be fixed by changing the name of the function to triple, or the integer to 2.
