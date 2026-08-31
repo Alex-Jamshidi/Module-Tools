d10f140e02ad4b3bb52b386dfb4051998c36f4f9
Alex Jamshidi <alexjamshidi@gmail.com>
2026-09-01 00:14:58 +0100

    exercise 2 complete

---
+++ Sprint 5 Prep Exercises/exercise_2.py
@@ -0,0 +1,11 @@
+def double(number):
+    return number * 3
+
+print(double(10))
+
+# Read the above code and write down what the bug is. How would you fix it?
+
+# Potential bug: code says double and it is multiplying by 3
+# We can't be sure this is a bug without knowing more information about intent of the developer or requirement of the user.
+
+# Assuming a bug; it could be fixed by changing the name of the function to triple, or the integer to 2.
