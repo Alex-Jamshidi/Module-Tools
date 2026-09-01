8922e287882376478d97325a18f3d5fd86c5e8bd
Alex Jamshidi <alexjamshidi@gmail.com>
2026-09-01 01:03:52 +0100

    exercise 8 completed

--- Sprint 5 Prep Exercises/exercise_8.py
+++ Sprint 5 Prep Exercises/exercise_8.py
@@ -1,11 +1,14 @@
+# Write a Person class using @datatype which uses a datetime.date for date of birth, rather than an int for age.
+from dataclasses import dataclass
 from datetime import date
 
+@dataclass(frozen=True)
 class Person:
-    def __init__(self, name: str, dob: date, preferred_operating_system: str):
-        self.name = name
-        self.dob = dob
-        self.preferred_operating_system = preferred_operating_system
+    name: str
+    dob: date
+    preferred_operating_system: str
 
+# Re-add the is_adult method to it.
     def is_adult(self):
         dob = self.dob
         today = date.today()
@@ -19,8 +22,3 @@ class Person:
 
 imran = Person("Imran", date(2004, 8, 31), "Ubuntu")
 print(imran.is_adult())
-
-# Change the Person class to take a date of birth
-# (using the standard library’s datetime.date class) and store it in a field instead of age.
-
-# Update the is_adult method to act the same as before.
