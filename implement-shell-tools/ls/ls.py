5c4328ed364bde62aacd0a6e63994062865f7140
Alex Jamshidi <alexjamshidi@gmail.com>
2026-09-12 07:20:24 +0100

    comments to PR

--- implement-shell-tools/ls/ls.py
+++ implement-shell-tools/ls/ls.py
@@ -14,6 +14,7 @@ parser.add_argument("file_system_items", nargs="*", help="Files or folders to di
 
 args = parser.parse_args()
 
+
 # ===== ls Procedure =====
 def ls(args):
     flag_status = {"print_in_list": False, "show_all": False}
@@ -24,7 +25,10 @@ def ls(args):
     dir_args = get_dir_args(cwd, fs_items)
     file_args = get_file_args(cwd, fs_items, flag_status)
 
-    print_output(populate_output(cwd, fs_items, dir_args, file_args, flag_status), flag_status)
+    print_output(
+        populate_output(cwd, fs_items, dir_args, file_args, flag_status), flag_status
+    )
+
 
 # ===== Flag Handling =====
 def execute_flags(flag_status):
@@ -33,8 +37,10 @@ def execute_flags(flag_status):
     if args.a:
         flag_status["show_all"] = True
 
+
 def make_list(output_string):
     return output_string.replace("\t", "\n").replace("\n\n", "\n")
+
 
 # Extracting Data from Arguments
 def check_args_length(fs_items):
@@ -42,22 +48,17 @@ def check_args_length(fs_items):
         fs_items.append(".")
     return fs_items
 
+
 def get_dir_args(cwd, fs_items):
-    return [
-        p
-        for p in fs_items
-        if (Path(cwd) / p).is_dir()
-    ]
+    return [p for p in fs_items if (Path(cwd) / p).is_dir()]
+
 
 def get_file_args(cwd, fs_items, flag_status):
-    file_args = [
-        p
-        for p in fs_items
-        if not (Path(cwd) / p).is_dir()
-    ]
+    file_args = [p for p in fs_items if not (Path(cwd) / p).is_dir()]
     if not flag_status["show_all"]:
         file_args = remove_dot_files(file_args)
     return file_args
+
 
 # Populating and Outputting Data
 def populate_output(cwd, fs_items, dir_args, file_args, flag_status):
@@ -72,8 +73,11 @@ def populate_output(cwd, fs_items, dir_args, file_args, flag_status):
         for file in file_args:
             output_string += file + "\t"
         for dir_item in dir_args:
-            output_string += f"\n\n{dir_item}:\n" + dir_output(dir_item, cwd, flag_status)
+            output_string += f"\n\n{dir_item}:\n" + dir_output(
+                dir_item, cwd, flag_status
+            )
     return output_string
+
 
 def dir_output(dir_item, cwd, flag_status):
     target_path = Path(cwd) / dir_item
@@ -88,14 +92,17 @@ def dir_output(dir_item, cwd, flag_status):
         output_str += item + "\t"
     return output_str
 
+
 def remove_dot_files(file_list):
     return [item for item in file_list if not item.startswith(".")]
+
 
 def print_output(output_string, flag_status):
     output = output_string
     if flag_status["print_in_list"]:
         output = make_list(output)
     print(output.rstrip())
+
 
 # ===== Run ls =====
 ls(parser.parse_args())
