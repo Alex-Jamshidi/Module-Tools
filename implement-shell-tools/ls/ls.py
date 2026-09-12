36b5584b706bc51b87892645134cd0eeda8c1d33
Alex Jamshidi <alexjamshidi@gmail.com>
2026-09-12 07:42:45 +0100

    removed cwd variable to use in built Path library functionaility

--- implement-shell-tools/ls/ls.py
+++ implement-shell-tools/ls/ls.py
@@ -20,13 +20,12 @@ def ls(args):
     flag_status = {"print_in_list": False, "show_all": False}
     execute_flags(flag_status)
 
-    cwd = os.getcwd()
     fs_items = check_args_length(args.file_system_items)
-    dir_args = get_dir_args(cwd, fs_items)
-    file_args = get_file_args(cwd, fs_items, flag_status)
+    dir_args = get_dir_args(fs_items)
+    file_args = get_file_args(fs_items, flag_status)
 
     print_output(
-        populate_output(cwd, fs_items, dir_args, file_args, flag_status), flag_status
+        populate_output(fs_items, dir_args, file_args, flag_status), flag_status
     )
 
 
@@ -49,38 +48,36 @@ def check_args_length(fs_items):
     return fs_items
 
 
-def get_dir_args(cwd, fs_items):
-    return [p for p in fs_items if (Path(cwd) / p).is_dir()]
+def get_dir_args(fs_items):
+    return [p for p in fs_items if Path(p).is_dir()]
 
 
-def get_file_args(cwd, fs_items, flag_status):
-    file_args = [p for p in fs_items if not (Path(cwd) / p).is_dir()]
+def get_file_args(fs_items, flag_status):
+    file_args = [p for p in fs_items if not Path(p).is_dir()]
     if not flag_status["show_all"]:
         file_args = remove_dot_files(file_args)
     return file_args
 
 
 # Populating and Outputting Data
-def populate_output(cwd, fs_items, dir_args, file_args, flag_status):
+def populate_output(fs_items, dir_args, file_args, flag_status):
     output_string = ""
 
     if len(fs_items) == 1:
         for file in file_args:
             output_string += file + " "
         for dir_item in dir_args:
-            output_string += dir_output(dir_item, cwd, flag_status)
+            output_string += dir_output(dir_item, flag_status)
     else:
         for file in file_args:
             output_string += file + "\t"
         for dir_item in dir_args:
-            output_string += f"\n\n{dir_item}:\n" + dir_output(
-                dir_item, cwd, flag_status
-            )
+            output_string += f"\n\n{dir_item}:\n" + dir_output(dir_item, flag_status)
     return output_string
 
 
-def dir_output(dir_item, cwd, flag_status):
-    target_path = Path(cwd) / dir_item
+def dir_output(dir_item, flag_status):
+    target_path = Path.cwd() / dir_item
     contents = os.listdir(target_path)
     output_str = ""
     if flag_status["show_all"]:
